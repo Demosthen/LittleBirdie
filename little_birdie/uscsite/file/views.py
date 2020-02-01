63be4b17952c010ecd8b05a70fdfeabef10420f0
Jennifer Zhou <jennifersiyuanzhou@berkeley.edu>
2020-02-01 08:27:41 -0800

    addded form; /post/ to upload and /read/ to get result

--- little_birdie/uscsite/file/views.py
+++ little_birdie/uscsite/file/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import file_upload
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+import os
+from .post_file import PostForm # new
+from django.urls import reverse_lazy # new
+
 
 
 # Create your views here.
@@ -11,11 +15,28 @@ class UploadPageView(ListView):
     model = file_upload
     template_name = 'upload.html'
 
+class CreatePostView(CreateView): # new
+    model = file_upload
+    form_class = PostForm
+    template_name = 'post_file.html'
+    success_url = '/read/'
+
+#def add_file(request):
+
+
 def read_file(request):
-    p = "/Users/administrator/Documents/GitHub/LittleBirdie/little_birdie/uscsite/media/text/test.txt"
-    f = open(p, 'r')
-    file_content = f.read()
-    f.close()
+    #p = "/Users/administrator/Documents/GitHub/LittleBirdie/little_birdie/uscsite/media/text/test.txt"
+    folder = "media/text"
+    file_content = "hello"
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            f = open(file_path, 'r')
+            file_content = f.read()
+            f.close()
+        except:
+            print('uh oh')
+    #"../media/text/test.txt"
     template = loader.get_template('upload.html')
     context = {
         'file_content': file_content
